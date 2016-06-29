0095943dc5c355660c71e7097b094a9d769a3ce5
Joan Mayer Campbell <joanmayercampbell@gmail.com>
2016-06-29 10:23:14 -0400

    added some classes with inheritence

---
+++ ExpressYourself/MediaItem.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpressYourself
+{
+    class MediaItem
+    {
+        public string Title { get; set; }
+        public string MediaType { get; set; }
+        public string Publisher { get; set; }
+
+        public MediaItem()
+        {
+            Title = null;
+            MediaType = null;
+        }
+
+    }
+
+    class BookType: MediaItem
+    {
+        public string Author { get; set; }
+        public int Length { get; set; }
+        public int CopyRight { get; set; }
+
+        public BookType()
+        {
+
+        }
+    }
+
+    class MagazineType: MediaItem
+    {
+       
+        public string Edition { get; set; }
+        public int Length { get; set; }
+
+        public MagazineType()
+        {
+
+        }
+
+    }
+
+    class DVDType: MediaItem
+    {
+        
+        public int Length { get; set; }
+
+        public DVDType()
+        {
+
+        }
+    }
+}
